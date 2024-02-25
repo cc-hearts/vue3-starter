e29de1ad23fd79098c7dffda215d171b44faace3
carl-chen <ccheartszzz@gmail.com>
2024-02-25 23:57:01 +0800

    fix: fix use theme export

--- src/utils/init-theme.ts
+++ src/utils/init-theme.ts
@@ -6,7 +6,7 @@ export function initTheme() {
   if (typeof document !== 'undefined') {
     const transition = document.body.style.transition
     document.body.style.transition = 'none'
-    const [, setThemeRef] = useTheme()
+    const [_, setThemeRef] = useTheme()
     setThemeRef(getTheme() === THEME.DARK ? THEME.DARK : THEME.LIGHT)
     document.body.offsetHeight
     document.body.style.transition = transition
