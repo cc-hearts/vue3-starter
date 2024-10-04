190a98fc863ae356c3e27f629a8b17afd3aed7da
Carl Chen <ccheartszzz@gmail.com>
2024-10-04 23:17:25 +0800

    feat: optimize test

--- src/__tests__/components/input/input-format-number.test.ts
+++ src/__tests__/components/input/input-format-number.test.ts
@@ -3,13 +3,43 @@ import { expect, describe, test } from 'vitest'
 import { mount } from '@vue/test-utils'
 
 describe('InputFormatNumber components', () => {
-  test('format number', async () => {
+  test('should format number with commas on prop change', async () => {
     const wrapper = mount(InputFormatNumber)
-
     await wrapper.setProps({ value: '123456' })
+    const inputEl = wrapper.find('input')
+    expect(inputEl.element.value).toBe('123,456')
+  })
 
+  test('should format number correctly when pasted', async () => {
+    const wrapper = mount(InputFormatNumber)
     const inputEl = wrapper.find('input')
 
+    await inputEl.setValue('123456')
+    inputEl.element.dispatchEvent(
+      new InputEvent('input', { inputType: 'insertFromPaste' }),
+    )
     expect(inputEl.element.value).toBe('123,456')
+  })
+
+  test('should format number correctly when inputting additional digits', async () => {
+    const wrapper = mount(InputFormatNumber)
+    const inputEl = wrapper.find('input')
+
+    await inputEl.setValue('123,456')
+    inputEl.element.dispatchEvent(
+      new InputEvent('input', { inputType: 'insertText', data: '123,4567' }),
+    )
+    expect(inputEl.element.value).toBe('1,234,567')
+  })
+
+  test('should format number correctly when deleting content', async () => {
+    const wrapper = mount(InputFormatNumber, { props: { value: '123,456' } })
+    const inputEl = wrapper.find('input')
+
+    await inputEl.setValue('123,4')
+    inputEl.element.dispatchEvent(
+      new InputEvent('input', { inputType: 'deleteContentBackward' }),
+    )
+    expect(inputEl.element.value).toBe('1,234')
   })
 })
